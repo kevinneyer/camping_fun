f0da251a1b9c90c5b0f5859225c0a5baee1e13d2
kevinneyer <kevinneyer@gmail.com>
2020-06-11 21:06:09 -0400

    One and Only

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :activities, only: [:index]
-  resources :campers, only: [:index]
+  # resources :activities
+  resources :campers
+  resources :signups
+  get '/activities', to: 'activities#index', as: "activities"
+  get '/activity/:id', to: 'activities#show', as: "activity"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_02_175322) do
+ActiveRecord::Schema.define(version: 2020_06_12_000600) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -32,8 +32,6 @@ ActiveRecord::Schema.define(version: 2019_01_02_175322) do
     t.integer "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["activity_id"], name: "index_signups_on_activity_id"
-    t.index ["camper_id"], name: "index_signups_on_camper_id"
   end
 
 end
